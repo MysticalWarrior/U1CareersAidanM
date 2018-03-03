09752eab7cc8f62df73fd8883f3016cc4b5c76fa
MysticalWarrior <36234902+MysticalWarrior@users.noreply.github.com>
2018-03-02 22:58:31 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿///Aidan McClung
+///3/2/2018
+///U1CareersAidanM
+///Provides a description of various features of YorkU's Computer security program.
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U1CareersAidanM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+    }
+}
